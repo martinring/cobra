c9ad8f20c2d7cb30784da8afee19daebfe4b7c43
Martin Ring <martin.ring@dfki.de>
2016-05-11 13:46:03 +0200

    simplify client

--- modules/cobra-common/shared/src/main/scala/net/flatmap/cobra/Client.scala
+++ modules/cobra-common/shared/src/main/scala/net/flatmap/cobra/Client.scala
@@ -3,12 +3,12 @@ package net.flatmap.cobra
 import scala.util.Success
 
 case class Client[T](val rev: Long, pending: Option[Operation[T]] = None, buffer: Option[Operation[T]] = None) {
-  def localEdit(operation: Operation[T]): (Boolean, Client[T]) = (pending,buffer) match {
-    case (None,None) => (true,Client(rev,Some(operation),None))
-    case (Some(p),None) => (false,Client(rev,Some(p),Some(operation)))
-    case (Some(p),Some(b)) => (false,Client(rev,Some(p),Some(Operation.compose(b,operation).get)))
-  } // (pending.isEmpty, Client(rev, pending orElse Some(operation), buffer.map(Operation.compose(_,operation).get)))
-    
+  def localEdit(operation: Operation[T]): (Boolean, Client[T]) = {
+    val newPending = pending orElse Some(operation)
+    val newBuffer = pending.map(_ => (buffer ++ Some(operation)).reduce(Operation.compose(_,_).get))
+    (pending.isEmpty,Client(rev,newPending,newBuffer))
+  }
+
   def remoteEdit(operation: Operation[T]): (Operation[T], Client[T]) = (pending,buffer) match {
     case (None,None) => (operation,Client(rev + 1, None, None))
     case (Some(p),None) =>
@@ -18,11 +18,7 @@ case class Client[T](val rev: Long, pending: Option[Operation[T]] = None, buffer
       val Success((a1,b1)) = Operation.transform(p,operation)
       val Success((a2,b2)) = Operation.transform(b,b1)
       (b2,Client(rev + 1, Some(a1), Some(a2)))
-  } /*{
-    val t1 = pending.map(Operation.transform(_, operation).get)
-    val t2 = buffer.flatMap(buf => t1.map { case (a1,b1) => Operation.transform(buf, b1).get } )
-    (t2.map(_._2) orElse t1.map(_._2) getOrElse operation, Client(rev + 1, t1.map(_._1), t2.map(_._1)))
-  }*/
+  }
 
   def ack: (Option[Operation[T]], Client[T]) =
     (buffer, Client(rev + 1, buffer, None))
