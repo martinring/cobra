8289429037aa076a2f9c794a80a1eab429617340
Martin Ring <martin.ring@dfki.de>
2016-05-12 14:58:58 +0200

    Persistent annotations

--- modules/cobra-server/src/main/scala/net/flatmap/cobra/SnippetServer.scala
+++ modules/cobra-server/src/main/scala/net/flatmap/cobra/SnippetServer.scala
@@ -2,7 +2,7 @@ package net.flatmap.cobra
 
 import akka.actor.{Actor, ActorLogging, ActorRef, Props, Terminated}
 import net.flatmap.cobra.isabelle.IsabelleService
-import net.flatmap.collaboration.{Document, Server}
+import net.flatmap.collaboration.{Annotations, Document, Server}
 
 import scala.util.{Failure, Success}
 
@@ -21,28 +21,35 @@ class SnippetServer(env: Map[String,String]) extends Actor with ActorLogging {
         isa ! ResetSnippet(id,content,0)
         Some(isa)
       } else None
-      context.become(initialized(mode, new Server(Document(content)), Set(sender) ++ service))
+      context.become(initialized(mode, new Server(Document(content)), Set(sender) ++ service,Map.empty))
       context.watch(sender)
   }
 
-  def initialized(mode: Mode, server: Server[Char], listeners: Set[ActorRef]): Receive = {
+  def initialized(mode: Mode, server: Server[Char], listeners: Set[ActorRef], annotations: Map[String,Annotations]): Receive = {
     case Terminated(listener) =>
       log.debug("client disconnected")
-      context.become(initialized(mode,server,listeners - listener))
+      context.become(initialized(mode,server,listeners - listener,annotations))
     case InitDoc(id,content,mode) =>
       log.debug("client connected")
       context.watch(sender)
-      context.become(initialized(mode,server,listeners + sender))
+      context.become(initialized(mode,server,listeners + sender,annotations))
       if (server.revision > 0) {
         sender ! CombinedRemoteEdit(id, server.getCombinedHistory, server.revision)
+        annotations.foreach { case (aid,as) =>
+          sender ! RemoteAnnotations(id,aid,as)
+        }
       }
     case Annotate(id,aid,as,rev) =>
+      context.become(initialized(mode,server,listeners,annotations + (aid -> as)))
       (listeners - sender).foreach(_ ! RemoteAnnotations(id,aid,as))
       log.info("annotated something")
     case Edit(id,op,rev) =>
       log.debug("applying edit")
       server.applyOperation(op,rev) match {
         case Success(op) =>
+          context.become(initialized(mode,server,listeners,annotations.map {
+            case (aid,as) => aid -> server.transformAnnotation(server.revision - 1, as).get
+          }))
           sender ! AcknowledgeEdit(id)
           (listeners - sender).foreach(_ ! RemoteEdit(id,op))
         case Failure(e) =>
