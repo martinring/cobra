97ec96d6f5e4f58f16d4fecdffe57bcc42e183fb
Martin Ring <martin.ring@dfki.de>
2016-06-16 14:11:40 +0200

    fix v

---
+++ release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [ $# = 2 ]; then
+  old=$(cat .version)
+  new="$1"
+  next="$2-SNAPSHOT"
+  echo "version was: $old"
+  echo "releasing version $new"
+  sed -i "s/\"$old\"/\"$new\"/g" build.sbt
+  sed -i "s/\"$old\"/\"$new\"/g" modules/cobra-server/src/main/scala/net/flatmap/cobra/Cobra.scala
+  sed -i "s/$old/$new/g" README.md
+  sed -i "s/$old/$new/g" .bintray-deb.yml .bintray-rpm.yml
+  git add build.sbt modules/cobra-server/src/main/scala/net/flatmap/cobra/Cobra.scala README.md
+  git commit -m "release version $new"
+  git tag -a "version-$new" -m "Version $new"
+  git push origin "version-$new"
+  git push
+  echo "setting version to $next"
+  sed -i "s/version\\s+:=\\s+\"$old\"/version := \"$new\"/g" build.sbt
+  sed -i "s/val\\s+version\\s+=\\s\"$old\"/val version = \"$new\"/g" modules/cobra-server/src/main/scala/net/flatmap/cobra/Cobra.scala
+  sed -i "s/$old/$next/g" .version
+else
+  echo "usage: ./release.sh <version> <next-version>"
+fi
