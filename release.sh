1c7cd2d43725ca4a674a696d8d4e769618136ce9
Martin Ring <martin.ring@dfki.de>
2016-06-16 14:33:14 +0200

    better release mechanism

--- release.sh
+++ release.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 if [ $# = 2 ]; then
-  old=$(cat .version)
+  old=$(sed "1q;d" .version)
+  before=$(sed "2q;d" .version)
   new="$1"
   next="$2-SNAPSHOT"
   echo "version was: $old"
   echo "releasing version $new"
   sed -i "s/\"$old\"/\"$new\"/g" build.sbt
   sed -i "s/\"$old\"/\"$new\"/g" modules/cobra-server/src/main/scala/net/flatmap/cobra/Cobra.scala
-  sed -i "s/$old/$new/g" README.md
+  sed -i "s/$before/$new/g" README.md
   sed -i "s/$old/$new/g" .bintray-deb.yml .bintray-rpm.yml
-  git add build.sbt modules/cobra-server/src/main/scala/net/flatmap/cobra/Cobra.scala README.md
+  git add build.sbt modules/cobra-server/src/main/scala/net/flatmap/cobra/Cobra.scala README.md .bintray-deb.yml .bintray-rpm.yml
   git commit -m "release version $new"
   git tag -a "version-$new" -m "Version $new"
   git push origin "version-$new"
@@ -18,6 +19,7 @@ if [ $# = 2 ]; then
   sed -i "s/version\\s+:=\\s+\"$old\"/version := \"$new\"/g" build.sbt
   sed -i "s/val\\s+version\\s+=\\s\"$old\"/val version = \"$new\"/g" modules/cobra-server/src/main/scala/net/flatmap/cobra/Cobra.scala
   sed -i "s/$old/$next/g" .version
+  sed -i "s/$before/$new/g" .version
 else
   echo "usage: ./release.sh <version> <next-version>"
 fi
